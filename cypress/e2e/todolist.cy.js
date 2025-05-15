80f919683ff197d42b87d72381bf855be22ef2dc
m e <duchatelle@gmail.com>
2025-05-15 12:48:18 +0200

    Initial commit

---
+++ cypress/e2e/todolist.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+describe("Todo-list", () =>{
+    // Ecrire le test ici
+    it("ajouter une seule tâche", ()=>{
+        // visiter le site à tester
+        cy.visit("https://dwils.github.io/todolist/")
+        cy.get("#taskInput").type("Tache 1")
+        cy.get('#addTaskButton').click()
+        cy.get("span").should("have.text", "Tache 1")
+    })
+})
